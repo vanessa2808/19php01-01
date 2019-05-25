94b47cb8f1b80d895fb0b967f59d084c3c79d8e4
vanessa2808 <yenrion9941@gmail.com>
2019-05-25 08:35:23 +0700

    all existing files

--- session1/a.php
+++ session1/a.php
@@ -1,11 +1,11 @@
 <?php
 	echo "<h2>Hello</h2>";
 ?>
-<link rel="stylesheet" type="text/css" href="../css/a.css">
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Apple</title>
+	<link rel="stylesheet" type="text/css" href="../css/a.css">
 </head>
 <body>
 <h1>Vanessa</h1>
