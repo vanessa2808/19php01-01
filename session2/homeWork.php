0e606f6910b7b8944915f34156ac28a2d1fd8b84
vanessa2808 <yenrion9941@gmail.com>
2019-05-27 17:22:54 +0700

    all existing files

--- session2/homeWork.php
+++ session2/homeWork.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" type="text/css" href="homeWork.css">
 <?php
 function drawTriangle(){
 		for ($i=1; $i <= 10 ; $i++) { 
@@ -17,6 +18,15 @@ function drawTriangle(){
 				echo " " . "*";
 			}
 			echo "<br/>";
+		}
+		echo "<br/>";
+		echo "<div class ='triangle'</div>";
+		$n = 10;
+		for($i=1;$i <= $n; $i++){
+				echo " ";
+			for($j = 1;$j <= 2*$i-1; $j++)
+				echo "*";
+				echo "<br/>";
 		}
 	}
 	echo drawTriangle();
