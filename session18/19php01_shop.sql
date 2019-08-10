99817c82cf48b8545d0a4b39b49cf2b0e1ba02b7
vanessa2808 <yenrion9941@gmail.com>
2019-08-10 18:25:38 +0700

    updated

--- session18/19php01_shop.sql
+++ session18/19php01_shop.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2.1
--- http://www.phpmyadmin.net
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Aug 09, 2019 at 07:11 PM
--- Server version: 5.7.27-0ubuntu0.16.04.1
--- PHP Version: 7.2.20-2+ubuntu16.04.1+deb.sury.org+1
+-- Host: 127.0.0.1
+-- Generation Time: Aug 10, 2019 at 01:24 PM
+-- Server version: 10.1.40-MariaDB
+-- PHP Version: 7.1.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -34,6 +36,17 @@ CREATE TABLE `comments` (
   `created` datetime NOT NULL,
   `status` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `user_id`, `product_id`, `content`, `created`, `status`) VALUES
+(35, 0, 0, 'Dep', '2019-08-10 10:53:05', 0),
+(39, 0, 0, 'ssss', '2019-08-10 11:36:07', 0),
+(40, 0, 0, 'xinh dep', '2019-08-10 11:36:33', 0),
+(41, 0, 0, 'raats dep', '2019-08-10 01:07:58', 0),
+(42, 0, 0, 'cmt', '2019-08-10 01:08:58', 0);
 
 -- --------------------------------------------------------
 
@@ -69,7 +82,7 @@ CREATE TABLE `order_details` (
 --
 
 CREATE TABLE `products` (
-  `id` int(10) NOT NULL,
+  `product_id` int(10) NOT NULL,
   `product_category_id` int(10) NOT NULL,
   `name` varchar(255) NOT NULL,
   `description` text NOT NULL,
@@ -78,6 +91,13 @@ CREATE TABLE `products` (
   `created` datetime NOT NULL,
   `updated` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `product_category_id`, `name`, `description`, `image`, `price`, `created`, `updated`) VALUES
+(3, 4, 'Iphone', 'goood', '5d4e23c5da356_yen.jpg', 200000000, '2019-08-10 03:54:13', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -97,7 +117,7 @@ CREATE TABLE `product_categories` (
 --
 
 CREATE TABLE `users` (
-  `id` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
   `role` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -109,6 +129,13 @@ CREATE TABLE `users` (
   `created` datetime NOT NULL,
   `updated` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `role`, `username`, `password`, `name`, `email`, `phone`, `birthday`, `avatar`, `created`, `updated`) VALUES
+(1, 'admin', 'yenrion9941@gmail.com', 'ed2b1f468c5f915f3f1cf75d7068baae', 'Nguyễn Thị Hồng Yến', 'hagiang12@gmail.com', '0376381262', '2019-08-07', 'yen.jpg', '2019-08-09 03:03:35', '0000-00-00 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -136,7 +163,7 @@ ALTER TABLE `order_details`
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`product_id`);
 
 --
 -- Indexes for table `product_categories`
@@ -148,7 +175,7 @@ ALTER TABLE `product_categories`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -158,32 +185,39 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `order_details`
 --
 ALTER TABLE `order_details`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `product_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `product_categories`
 --
 ALTER TABLE `product_categories`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
